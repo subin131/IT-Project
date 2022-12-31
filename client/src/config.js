5d51bcf940b712e3e2fd26269466fce774e6fb45
satyal <satyalsubin33@gmail.com>
2022-12-31 22:36:05 +0545

    adding the base url

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+let BASE_URL = "http://localhost:5000/";
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//   BASE_URL = "http://localhost:5000/";
+// }
+
+export { BASE_URL };
