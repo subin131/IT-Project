550bc56af0fcdb42c91b7ecc3fcb2b8d52f1cc6b
satyal <satyalsubin33@gmail.com>
2023-01-03 20:18:35 +0545

    adding discover spaces

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Container } from "@mui/material";
 import Navbar from "./Navbar";
+import CreateSpace from "./CreateSpace";
+import { Button, Card, Link, Stack, Typography } from "@mui/material";
+import SortBySelect from "./SortBySelect";
+import HorizontalStack from "./util/HorizontalStack";
+import SortByRole from "./SortByRole";
+import DiscoverSpace from "./DiscoverSpace";
+import ViewSpace from "./ViewSpace";
 
 function Community() {
   return (
     <Container>
       <Navbar />
+      <Card>
+        <HorizontalStack justifyContent="space-between">
+          <CreateSpace />
+          <ViewSpace />
+        </HorizontalStack>
+      </Card>
+      <div style={{ marginTop: "10px" }}>
+        <DiscoverSpace />
+      </div>
     </Container>
   );
 }
