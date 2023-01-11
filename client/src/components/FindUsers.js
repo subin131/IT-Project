3adb42e69cce0d60913e02cac1f731ed13682123
satyal <satyalsubin33@gmail.com>
2023-01-11 23:08:28 +0545

    finding other users

---
+++ client/src/components/FindUsers.js
@@ -0,0 +1,78 @@
+import {
+  Avatar,
+  Card,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { RiGroupFill } from "react-icons/ri";
+import { MdRefresh } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { getRandomUsers } from "../api/users";
+import Loading from "./Loading";
+import UserAvatar from "./UserAvatar";
+import HorizontalStack from "./util/HorizontalStack";
+import { GrView } from "react-icons/gr";
+
+const FindUsers = () => {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState(null);
+
+  const fetchUsers = async () => {
+    setLoading(true);
+    const data = await getRandomUsers({ size: 5 });
+    setLoading(false);
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const handleClick = () => {
+    fetchUsers();
+  };
+
+  return (
+    <Card>
+      <Stack spacing={2}>
+        <HorizontalStack justifyContent="space-between">
+          <HorizontalStack>
+            <RiGroupFill />
+            <Typography>Find Others</Typography>
+          </HorizontalStack>
+          <IconButton
+            sx={{ padding: 0 }}
+            disabled={loading}
+            onClick={handleClick}
+          >
+            <MdRefresh />
+          </IconButton>
+        </HorizontalStack>
+
+        <Divider />
+
+        {loading ? (
+          <Loading />
+        ) : (
+          users &&
+          users.map((user) => (
+            <HorizontalStack justifyContent="space-between" key={user._id}>
+              <HorizontalStack>
+                <UserAvatar width={30} height={30} username={user.username} />
+                <Typography>{user.username}</Typography>
+              </HorizontalStack>
+              <Link to={"/users/" + user.username}>
+                <GrView />
+              </Link>
+            </HorizontalStack>
+          ))
+        )}
+      </Stack>
+    </Card>
+  );
+};
+
+export default FindUsers;
