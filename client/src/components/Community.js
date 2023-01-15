1e64bd8deeac587706181978ad51153f0a840742
satyal <satyalsubin33@gmail.com>
2023-01-15 23:21:28 +0545

    adding the community section

---
+++ client/src/components/Community.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "@mui/material";
+import Navbar from "./Navbar";
+
+function Community() {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+}
+
+export default Community;
