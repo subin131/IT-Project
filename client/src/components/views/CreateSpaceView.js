464e9c178b07bfde16c685acc1aa9b0fcaaecd95
satyal <satyalsubin33@gmail.com>
2023-01-21 23:54:44 +0545

    adding the component of creating space

---
+++ client/src/components/views/CreateSpaceView.js
@@ -0,0 +1,26 @@
+import { Container } from "@mui/material";
+import React from "react";
+import GoBack from "../GoBack";
+import GridLayout from "../GridLayout";
+import Navbar from "../Navbar";
+import PostEditor from "../PostEditor";
+import Sidebar from "../Sidebar";
+import { Button, Card, Link, Stack, Typography } from "@mui/material";
+import CreateSpace from "../CreateSpace";
+import HorizontalStack from "../util/HorizontalStack";
+import SortBySelect from "../SortBySelect";
+const CreateSpaceView = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Card>
+        <HorizontalStack justifyContent="space-between">
+          <CreateSpace />
+          <SortBySelect />
+        </HorizontalStack>
+      </Card>
+    </Container>
+  );
+};
+
+export default CreateSpaceView;
