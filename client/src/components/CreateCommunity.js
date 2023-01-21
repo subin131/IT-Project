366e6d1a7cf00d6e3671e7f64ad39fa07496dd3f
satyal <satyalsubin33@gmail.com>
2023-01-21 23:55:50 +0545

    updates

---
+++ client/src/components/CreateCommunity.js
@@ -0,0 +1,29 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { MdOutlineAddCircle } from "react-icons/md";
+
+const CreateCommunity = () => {
+  const navigate = useNavigate();
+  return (
+    <Button
+      style={{
+        color: "#00d5fa",
+
+        border: "1px solid #00d5fa",
+      }}
+      variant="outlined"
+      size="medium"
+      //   onClick={() => navigate("/posts/create")}
+      sx={{
+        gap: "0.2rem",
+        whiteSpace: "nowrap",
+      }}
+    >
+      <MdOutlineAddCircle style={{ flexShrink: 0 }} />
+      <span> Create Community</span>
+    </Button>
+  );
+};
+
+export default CreateCommunity;
