d2273e0a9a7e1a02c1c17a8a5e8e8f86b29f3d11
satyal <satyalsubin33@gmail.com>
2023-01-23 08:21:39 +0545

    adding the uploader in route

--- routes/posts.js
+++ routes/posts.js
@@ -2,27 +2,14 @@ const express = require("express");
 const router = express.Router();
 const postControllers = require("../controllers/postControllers");
 const { verifyToken, optionallyVerifyToken } = require("../middleware/auth");
-const multer = require("multer");
 
-//storage engine
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-//init upload
-const upload = multer({
-  storage: storage,
-});
+//importing the uploader middleware
+const uploader = require("../middleware/uploader");
 router.get("/", optionallyVerifyToken, postControllers.getPosts);
 //post request to create a post
 router.post(
   "/",
-  upload.single("picturePath"),
+  uploader.single("image"),
   verifyToken,
   postControllers.createPost
 );
