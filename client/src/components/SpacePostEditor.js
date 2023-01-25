46180ed13eee2ef5244f3e63660b12680c878cc1
satyal <satyalsubin33@gmail.com>
2023-01-25 20:44:09 +0545

    adding editor part

---
+++ client/src/components/SpacePostEditor.js
@@ -0,0 +1,132 @@
+import {
+  Button,
+  Card,
+  // Link,
+  Stack,
+  TextField,
+  // IconButton,
+  Typography,
+  // Divider,
+} from "@mui/material";
+
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createPost } from "../api/posts";
+import ErrorAlert from "./ErrorAlert";
+import { isLoggedIn } from "../helpers/authHelper";
+import HorizontalStack from "./util/HorizontalStack";
+import UserAvatar from "./UserAvatar";
+
+const SpacePostEditor = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    picturePath: "",
+  });
+
+  const [serverError, setServerError] = useState("");
+  const [errors, setErrors] = useState({});
+  const user = isLoggedIn();
+
+  const imageHandleChange = (e) => {
+    setImage(e.target.files[0]);
+    console.log("image", image.name);
+  };
+
+  // console.log("image form", imageURL);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+      picturePath: image,
+    });
+    const errors = validate();
+    setErrors(errors);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const data = await createPost(formData, isLoggedIn());
+    setLoading(false);
+    if (data && data.error) {
+      setServerError(data.error);
+    } else {
+      navigate("/posts/" + data._id);
+    }
+    setImage(null);
+    console.log(formData);
+  };
+
+  const validate = () => {
+    const errors = {};
+
+    return errors;
+  };
+
+  return (
+    <Card>
+      <Stack spacing={1}>
+        {user && (
+          <HorizontalStack spacing={2}>
+            <UserAvatar width={50} height={50} username={user.username} />
+            <Typography variant="h5">
+              What would you like to post today in your space {user.username}?
+            </Typography>
+          </HorizontalStack>
+        )}
+
+        <Box component="form" onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            label="Title"
+            required
+            name="title"
+            margin="normal"
+            onChange={handleChange}
+            error={errors.title !== undefined}
+            helperText={errors.title}
+          />
+          <TextField
+            fullWidth
+            label="Content"
+            multiline
+            rows={10}
+            name="content"
+            margin="normal"
+            onChange={handleChange}
+            error={errors.content !== undefined}
+            helperText={errors.content}
+            required
+          />
+          <TextField
+            type="file"
+            name="picturePath"
+            onChange={imageHandleChange}
+          />
+
+          <ErrorAlert error={serverError} />
+          <Button
+            style={{ color: "#00d5fa", borderColor: "#00d5fa" }}
+            variant="outlined"
+            type="submit"
+            fullWidth
+            disabled={loading}
+            sx={{
+              mt: 2,
+            }}
+          >
+            {loading ? <>Submitting</> : <>Submit</>}
+          </Button>
+        </Box>
+      </Stack>
+    </Card>
+  );
+};
+
+export default SpacePostEditor;
