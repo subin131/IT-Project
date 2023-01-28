5fe603e1bbcf375c15f7c8923cf4a80ab4fabf1f
satyal <satyalsubin33@gmail.com>
2023-01-28 17:17:39 +0545

    adding changes in code

--- routes/spaces.js
+++ routes/spaces.js
@@ -6,6 +6,8 @@ const { verifyToken, optionallyVerifyToken } = require("../middleware/auth");
 router.get("/", optionallyVerifyToken, spaceControllers.getSpaces);
 router.post("/", verifyToken, spaceControllers.createSpace);
 
-router.get("/:id", optionallyVerifyToken, spaceControllers.getSpaces);
+router.get("/:id", optionallyVerifyToken, spaceControllers.getSpace);
 
+router.patch("/:id", verifyToken, spaceControllers.updateSpace);
+router.delete("/:id", verifyToken, spaceControllers.deleteSpace);
 module.exports = router;
