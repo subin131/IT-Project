b43d8be51374b3e5ba2053486cc94d9ad20bd6eb
satyal <satyalsubin33@gmail.com>
2023-02-03 09:52:48 +0545

    image uploader middleware changes

--- middleware/uploader.js
+++ middleware/uploader.js
@@ -4,15 +4,10 @@ const crypto = require("crypto");
 
 const myStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // let path = process.cwd() + "/uploads";
     cb(null, "./uploads/");
   },
-  // filename: (req, file, cb) => {
-  //   let f_name = new Date().toISOString() + "-" + file.originalname;
-  //   cb(null, f_name);
-  // },
+
   filename: (req, file, callback) => {
-    //this is just setting a unique filename
     let temp = file.originalname.split(".");
     const filename = crypto.randomBytes(16).toString("hex") + "." + temp[1];
     callback(null, filename);
